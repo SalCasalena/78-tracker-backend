b36a3e833c51cf80740218593405d3d7b08c282b
Sal Casalena <salcasalena@gmail.com>
2025-02-22 21:07:48 -0500

    Auth Part1

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username + ": " + self.email
