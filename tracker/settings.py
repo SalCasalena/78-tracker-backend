8586706851facc342af053d346cd2767453c35bc
Sal Casalena <salcasalena@gmail.com>
2025-03-03 09:53:58 -0500

    DNS config support

--- tracker/settings.py
+++ tracker/settings.py
@@ -29,7 +29,13 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    ".herokuapp.com", 
+    "localhost", 
+    "127.0.0.1",
+    "api.seventyeight.life",
+    "seventyeight.life",
+]
 
 
 # Application definition
