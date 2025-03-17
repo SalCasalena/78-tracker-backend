c81c4a7d790851166dbabe4047a4f1b309103080
Sal Casalena <salcasalena@gmail.com>
2025-03-16 22:10:19 -0400

    Fixed Serizlizer to distiquish teams

--- games/serializers.py
+++ games/serializers.py
@@ -19,9 +19,36 @@ class GameStateSerializer(serializers.ModelSerializer):
         ]
 
     def get_player_stats(self, obj):
-        """Retrieve player statistics for the game and return as JSON."""
+        """Retrieve player statistics grouped by team."""
+        # Fetch all player stats for the given game
         player_stats = PlayerStats.objects.filter(game=obj)
-        return {str(stat.player.pk): PlayerStatsSerializer(stat).data for stat in player_stats}
+        
+        # Group players into Team A and Team B
+        teamA_players = [
+            obj.team1.player1, obj.team1.player2, obj.team1.player3,
+            obj.team1.player4, obj.team1.player5, obj.team1.player6
+        ]
+        teamB_players = [
+            obj.team2.player1, obj.team2.player2, obj.team2.player3,
+            obj.team2.player4, obj.team2.player5, obj.team2.player6
+        ]
+        
+        # Build the grouped response
+        grouped_stats = {
+            "teamA": {},
+            "teamB": {}
+        }
+
+        for stat in player_stats:
+            player_id = str(stat.player.pk)
+            serialized_data = PlayerStatsSerializer(stat).data
+
+            if stat.player in teamA_players:
+                grouped_stats["teamA"][player_id] = serialized_data
+            elif stat.player in teamB_players:
+                grouped_stats["teamB"][player_id] = serialized_data
+
+        return grouped_stats
 
 
 class RoundResponseSerializer(serializers.ModelSerializer):
